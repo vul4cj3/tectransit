1f47f1eb9e04c229753a06ce622f55bc8665a187
Jiao Rong Wu <vul4cj3@gmail.com>
2020-02-03 11:12:23 +0800

    修正Router問題；其他設定修改

--- Tectransit/Startup.cs
+++ Tectransit/Startup.cs
@@ -39,10 +39,10 @@ namespace Tectransit
             services.TryAddSingleton<IActionContextAccessor, ActionContextAccessor>();
 
             // In production, the Angular files will be served from this directory
-            services.AddSpaStaticFiles(configuration =>
-            {
-                configuration.RootPath = "TecTransit/dist";
-            });
+            //services.AddSpaStaticFiles(configuration =>
+            //{
+            //    configuration.RootPath = "TecTransit/dist/TecTransit";
+            //});
 
             services.Configure<ForwardedHeadersOptions>(options =>
             {
@@ -69,16 +69,16 @@ namespace Tectransit
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseSpaStaticFiles();
+            //app.UseSpaStaticFiles();
             
             app.UseStaticFiles(new StaticFileOptions()
             {
-                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"admin/src/assets")),
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"admin/dist/admin/assets")),
                 RequestPath = new PathString("/assets")
             });
             app.UseStaticFiles(new StaticFileOptions()
             {
-                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"TecTransit/src/assets")),
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"TecTransit/dist/TecTransit/assets")),
                 RequestPath = new PathString("/res/assets")
             });
 
@@ -107,22 +107,59 @@ namespace Tectransit
             //router setting
             app.Map("/admin", admin =>
             {
+                string clientApp1Path = env.IsDevelopment() ? "admin" : @"admin/dist/admin";
+
+                // Each map gets its own physical path for it to map the static files to. 
+                StaticFileOptions clientApp1Dist = new StaticFileOptions()
+                {
+                    FileProvider = new PhysicalFileProvider(
+                            Path.Combine(Directory.GetCurrentDirectory(), clientApp1Path)
+                        )
+                };
+
+                // Each map its own static files otherwise it will only ever serve index.html no matter the filename 
+                admin.UseSpaStaticFiles(clientApp1Dist);
                 admin.UseSpa(spa =>
                 {
                     spa.Options.SourcePath = "admin";
                     if (env.IsDevelopment())
                     {
+                        // it will use package.json & will search for start command to run
                         spa.UseAngularCliServer(npmScript: "start");
+                        //spa.Options.DefaultPageStaticFileOptions = clientApp1Dist;
+                    }
+                    else
+                    {
+                        spa.Options.DefaultPageStaticFileOptions = clientApp1Dist;
                     }
                 });
             }).Map("", client =>
             {
+
+                string clientApp2Path = env.IsDevelopment() ? "TecTransit" : @"TecTransit/dist/TecTransit";
+
+                // Each map gets its own physical path for it to map the static files to. 
+                StaticFileOptions clientApp1Dist = new StaticFileOptions()
+                {
+                    FileProvider = new PhysicalFileProvider(
+                            Path.Combine(Directory.GetCurrentDirectory(), clientApp2Path)
+                        )
+                };
+
+                // Each map its own static files otherwise it will only ever serve index.html no matter the filename 
+                client.UseSpaStaticFiles(clientApp1Dist);
                 client.UseSpa(spa =>
                 {
                     spa.Options.SourcePath = "TecTransit";
                     if (env.IsDevelopment())
                     {
+                        // it will use package.json & will search for start command to run
                         spa.UseAngularCliServer(npmScript: "start");
+                        //spa.Options.DefaultPageStaticFileOptions = clientApp1Dist;
+                    }
+                    else
+                    {
+                        spa.Options.DefaultPageStaticFileOptions = clientApp1Dist;
                     }
                 });
             });
